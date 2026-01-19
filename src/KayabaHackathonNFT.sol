e739293258d56a15ac50ccb344296e921c823282
John Okyere <112992966+mhiskall282@users.noreply.github.com>
2026-01-19 17:37:57 +0000

    Update KayabaHackathonNFT.sol

--- src/KayabaHackathonNFT.sol
+++ src/KayabaHackathonNFT.sol
@@ -77,6 +77,9 @@
         
         // Return different metadata based on achievement level
         AchievementLevel level = hackathonInfo[tokenId].level;
+        if (level == AchievementLevel.WINNER) return _winnerMetadataURI;
+        if (level == AchievementLevel.RUNNER_UP) return _runnerupMetadataURI;
+        if (level == AchievementLevel.FINALIST) return _finalistMetadataURI;
         public
         view
         override(ERC721, ERC721URIStorage)
