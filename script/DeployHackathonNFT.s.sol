5334df236ee17496052f6e77624d119ed012f040
John Okyere <112992966+mhiskall282@users.noreply.github.com>
2026-01-19 17:57:30 +0000

    Update DeployHackathonNFT.s.sol

--- script/DeployHackathonNFT.s.sol
+++ script/DeployHackathonNFT.s.sol
@@ -1,3 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import "forge-std/Script.sol";
+import "../src/KayabaHackathonNFT.sol";
+
+contract DeployHackathonNFT is Script {
+    function run() external {
